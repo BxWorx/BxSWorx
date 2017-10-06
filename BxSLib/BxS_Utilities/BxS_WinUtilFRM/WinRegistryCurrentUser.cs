1604cbe25c9d7edc0da50b41194e33ad0705fd68
Derrick Bingham <dbcm@mweb.co.za>
2017-10-06 11:18:06 +0200

    update

---
+++ BxSLib/BxS_Utilities/BxS_WinUtilFRM/WinRegistryCurrentUser.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.Win32;
+//•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+namespace BxS_WinUtilFRM
+{
+	public class WinRegistryCurrentUser
+		{
+			#region **[Definitions]**
+
+			#endregion
+			//___________________________________________________________________________________________
+			#region **[Constructors]**
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				public WinRegistryCurrentUser(string rootNode	= "Software", string applicationName = "BxSWorx")
+					{
+						this.RootNode					= rootNode;
+						this.ApplicationName	= applicationName;
+					}
+			#endregion
+			//___________________________________________________________________________________________
+			#region **[Properties]**
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				public string RootNode
+					{	get; set; }
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				public string ApplicationName
+					{	get; set; }
+
+			#endregion
+			//___________________________________________________________________________________________
+			#region **[Methods: Exposed]**
+
+				//-------------------------------------------------------------------------------------------
+				public bool ExistsPath()
+					{
+						return	this.ExistsPath(this.Path());
+					}
+
+				//-------------------------------------------------------------------------------------------
+				public bool ExistsPath(string Path)
+					{
+						using (RegistryKey rootKey = Registry.CurrentUser)
+							{
+								return	rootKey?.OpenSubKey(Path, false) != null;
+							}
+					}
+
+				//-------------------------------------------------------------------------------------------
+				public bool Exists(string valueID)
+					{
+						return	this.Exists(this.Path(), valueID);
+					}
+
+				//-------------------------------------------------------------------------------------------
+				public bool Exists(string Path,	string valueID)
+					{
+						using (RegistryKey rootKey = Registry.CurrentUser)
+							{
+								try
+									{
+										RegistryKey subKey = rootKey?.OpenSubKey(Path, false);
+										return subKey == null ? false : subKey.GetValue(valueID) != null;
+									}
+								catch (System.Exception)
+									{	return false; }
+							}
+					}
+
+				//-------------------------------------------------------------------------------------------
+				public bool RemovePath()
+					{
+						return	this.RemovePath(this.Path());
+					}
+
+				//-------------------------------------------------------------------------------------------
+				public bool RemovePath(string Path)
+					{
+						using (RegistryKey rootKey = Registry.CurrentUser)
+							{
+								try
+									{
+										rootKey?.DeleteSubKeyTree(Path);
+										return	true;
+									}
+								catch (System.Exception)
+									{	return	false; }
+							}
+					}
+
+				//-------------------------------------------------------------------------------------------
+				public bool Remove(string valueID)
+					{
+						return	this.Remove(this.Path(), valueID);
+					}
+
+				//-------------------------------------------------------------------------------------------
+				public bool Remove(string Path, string valueID)
+					{
+						using (RegistryKey rootKey = Registry.CurrentUser)
+							{
+								try
+									{
+										RegistryKey subKey = rootKey?.OpenSubKey(Path, true);
+										subKey?.DeleteValue(valueID, true);
+										return	true;
+									}
+								catch (System.Exception)
+									{	return	false; }
+							}
+					}
+
+				//-------------------------------------------------------------------------------------------
+				public bool Write<T>(string valueID, T value)
+					{
+						return	this.Write(this.Path(), valueID, value);
+					}
+
+				//-------------------------------------------------------------------------------------------
+				public bool Write<T>(string Path,	string valueID, T value)
+					{
+						using (RegistryKey rootKey = Registry.CurrentUser)
+							{
+								try
+									{
+										RegistryKey rk = rootKey.CreateSubKey(Path);
+										rk.SetValue(valueID, value);
+										return	true;
+									}
+								catch (System.Exception)
+									{	return	false; }
+							}
+					}
+
+				//-------------------------------------------------------------------------------------------
+				public T Read<T>(string valueID, T defaultValue)
+					{
+						return	this.Read(this.Path(), valueID, defaultValue);
+					}
+
+				//-------------------------------------------------------------------------------------------
+				public T Read<T>(string Path,	string valueID, T defaultValue)
+					{
+						using (RegistryKey rootKey = Registry.CurrentUser)
+							{
+								try
+									{
+										RegistryKey subKey = rootKey?.OpenSubKey(Path, false);
+										return subKey == null ? defaultValue : (T)subKey.GetValue(valueID, defaultValue);
+									}
+								catch (System.Exception)
+									{	return defaultValue; }
+							}
+					}
+
+			#endregion
+			//___________________________________________________________________________________________
+			#region **[Methods: Private]**
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				private string Path()
+					{
+						return	$@"{this.RootNode}\{this.ApplicationName}";
+					}
+
+			#endregion
+		}
+}
--- BxSLib/BxS_Utilities/zBxS_WinUtil_UT/UT_BxS_WinUtil_Registry.cs
+++ BxSLib/BxS_Utilities/zBxS_WinUtil_UT/UT_BxS_WinUtil_Registry.cs
@@ -7,28 +7,72 @@ namespace zBxS_WinUtil_UT
 	[TestClass]
 	public class UT_BxS_WinUtil_Registry
 	{
+		private const string	RootNode	= "Software";
+		private const string	AppID			= "BxSWorx";
+		private const string	ValIDOK		= "BxSTest";
+
+		//-------------------------------------------------------------------------------------------
 		[TestMethod]
 		public void UT_WinRegistryReadWriteRemove()
 			{
-				const string registryPath			= @"Software\BxS";
+				BxS_WinUtilFRM.WinRegistryCurrentUser registry = new BxS_WinUtilFRM.WinRegistryCurrentUser(RootNode, AppID);
 
-				const string registryNameErr	= "wwww";
-				const string registryNameOK		= "BxSTest";
-				const string registryDefVal		= "AAAA";
-				const string registryNewVal		= "XXXX";
+				if (registry.ExistsPath())	registry.RemovePath();
 
-				BxS_WinUtilFRM.WinRegistry registry = new BxS_WinUtilFRM.WinRegistry();
+				Assert.AreEqual(true	, registry.Write(ValIDOK, true)	,	"CRUD: Write: Error");
+				Assert.AreEqual(true	, registry.Exists(ValIDOK)			,	"CRUD: Read:	Error");
+				Assert.AreEqual(true	, registry.Remove(ValIDOK)			,	"CRUD: Delete1: Error");
+				Assert.AreEqual(false	, registry.Remove(ValIDOK)			,	"CRUD: Delete2: Error");
+				Assert.AreEqual(true	, registry.RemovePath()					,	"CRUD: Delete3: Error");
+			}
 
-				var regvalErr = registry.Read(registryPath, registryNameErr	, registryDefVal);
-				Assert.AreEqual			(regvalErr,	registryDefVal, "Read: Error");
+		//-------------------------------------------------------------------------------------------
+		[TestMethod]
+		public void UT_WinRegistryBase_Existing()
+			{
+				const string 	AppName		= @"Microsoft\Notepad";
+				const string	ValID			= "iWindowPosDX";
 
-				registry.Write(registryPath, registryNameOK, registryNewVal);
-				var regvalOK	= registry.Read(registryPath, registryNameOK	, registryDefVal);
-				Assert.AreEqual	(regvalOK	,	registryNewVal, "Write: Error");
+				var Path = string.Join(@"\", RootNode, AppName);
 
-				registry.Remove(registryPath, registryNameOK);
-				var regvalRem = registry.Read(registryPath, registryNameOK	, registryDefVal);
-				Assert.AreEqual	(regvalRem,	registryDefVal, "Remove: Error");
+				BxS_WinUtilFRM.WinRegistryCurrentUser registry0 = new BxS_WinUtilFRM.WinRegistryCurrentUser();
+				BxS_WinUtilFRM.WinRegistryCurrentUser registry2 = new BxS_WinUtilFRM.WinRegistryCurrentUser();
+				BxS_WinUtilFRM.WinRegistryCurrentUser registry1 = new BxS_WinUtilFRM.WinRegistryCurrentUser(RootNode, AppName);
+
+				registry2.ApplicationName	= AppName;
+				registry2.RootNode				= RootNode;
+
+				var VDwrd0	= registry0.Read(Path,	ValID, default(int));
+				var VDwrd1	= registry1.Read(				ValID, default(int));
+				var VDwrd2	= registry2.Read(				ValID, default(int));
+
+				Assert.AreNotEqual(default(int)	, VDwrd0	, "Base: DWrd0 error");
+				Assert.AreNotEqual(default(int)	, VDwrd1	, "Base: DWrd1 error");
+				Assert.AreNotEqual(default(int)	, VDwrd2	, "Base: DWrd1 error");
+
+				Assert.AreEqual(true	, registry0.Exists(Path,	ValID)	, "Base: Exists0 error");
+				Assert.AreEqual(true	, registry1.Exists(				ValID)	, "Base: Exists1 error");
+				Assert.AreEqual(true	, registry2.Exists(				ValID)	, "Base: Exists2 error");
+				Assert.AreEqual(false	, registry2.Exists(				"XXX")	, "Base: Exists3 error");
+
+				Assert.AreEqual(true	, registry0.ExistsPath(Path)	, "Base: ExistsPath0 error");
+				Assert.AreEqual(true	, registry1.ExistsPath()			, "Base: ExistsPath1 error");
+				Assert.AreEqual(false	, registry2.ExistsPath("XXX")	, "Base: ExistsPath2 error");
+			}
+
+		//-------------------------------------------------------------------------------------------
+		[TestMethod]
+		public void UT_WinRegistryBase_Defaults()
+			{
+				BxS_WinUtilFRM.WinRegistryCurrentUser registry = new BxS_WinUtilFRM.WinRegistryCurrentUser();
+
+				var VBool	= registry.Read(ValIDOK, default(bool));
+				var VStr	= registry.Read(ValIDOK, string.Empty);
+				var VInt	= registry.Read(ValIDOK,	default(int));
+
+				Assert.AreEqual(default(bool)	, VBool	, "Base: Bool error");
+				Assert.AreEqual(string.Empty	, VStr	, "Base: String error");
+				Assert.AreEqual(default(int)	, VInt	, "Base: Int error");
 			}
 	}
 }
