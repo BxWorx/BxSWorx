90fa4913fb03835d04ee8b90ab3dfd35bb54d93a
Derrick Bingham <dbcm@mweb.co.za>
2017-10-20 05:08:00 +0200

    update

---
+++ BxSLib/BxS_Utilities/BxS_SAPGUI/Code/SAPGUI_BxS/Handlers/INIController.cs
@@ -0,0 +1,42 @@
+﻿using SAPGUI.API;
+//•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+namespace SAPGUI.XML
+{
+	internal class BxSController : IControllerSource
+		{
+
+			//===========================================================================================
+			#region "Definitions"
+
+				//private readonly	XMLRepository		_Repository;
+
+			#endregion
+
+			//===========================================================================================
+			#region "Methods: Exposed"
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				public void GetConnection(IDTOConnection dtoConnection)
+					{
+						//this._Repository.LoadConnectionDTO(dtoConnection);
+					}
+
+			#endregion
+
+			//===========================================================================================
+			#region "Constructors"
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				public BxSController(string fullPath, bool onlySAPGUI)
+					{
+						//this._Repository	= new XMLParse2ReposDTO().Load(fullPath, onlySAPGUI);
+					}
+
+			#endregion
+
+			//===========================================================================================
+			#region "Methods: Private"
+			#endregion
+
+		}
+}
