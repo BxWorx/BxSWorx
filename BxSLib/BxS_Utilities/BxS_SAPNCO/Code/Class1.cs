660db4c9a130410c8f35e698f2f2c9b26c00b49b
Derrick Bingham <dbcm@mweb.co.za>
2017-12-04 17:42:08 +0200

    update

---
+++ BxSLib/BxS_Utilities/BxS_SAPNCO/Code/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SMC = SAP.Middleware.Connector;
+
+namespace BxS_SAPNCO
+{
+		public class Class1
+		{
+			private SMC.RfcConfigParameters				x = new SMC.RfcConfigParameters();
+			private SMC.SapLogonIniConfiguration	t = SMC.SapLogonIniConfiguration.Create();
+
+
+			public string[] tree()
+				{
+					return	this.t.GetEntries();
+				}
+
+		}
+}
---
+++ BxSLib/BxS_Utilities/zBxS_SAPNCO_UT/Unit Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace zBxS_SAPNCO_UT
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
