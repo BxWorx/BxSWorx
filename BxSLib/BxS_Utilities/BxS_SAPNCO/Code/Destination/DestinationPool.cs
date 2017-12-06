c8b59b8e566d1c18ee4e3e2b0231658acd10e9c0
Derrick Bingham <dbcm@mweb.co.za>
2017-12-06 11:00:27 +0200

    update

---
+++ BxSLib/BxS_Utilities/BxS_SAPNCO/Code/Destination/DestinationPool.cs
@@ -0,0 +1,41 @@
+﻿using SMC	= SAP.Middleware.Connector;
+using SDM = SAP.Middleware.Connector.RfcDestinationManager;
+//•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+namespace BxS_SAPNCO.Destination
+{
+	internal class DestinationPool
+		{
+			#region "Properties"
+
+				internal	
+
+
+				internal bool IsRegistered	{ get { return	SDM.IsDestinationConfigurationRegistered(); } }
+
+			#endregion
+
+			//===========================================================================================
+			#region "Methods: Exposed"
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				internal void Register(DestinationRepository destinationRepository)
+					{
+						SDM.RegisterDestinationConfiguration(destinationRepository);
+					}
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				internal SMC.RfcDestination	GetRfcDestination(string destinationName)
+					{
+						return	SDM.GetDestination(destinationName);
+					}
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				internal SMC.RfcDestination	GetRfcDestination(SMC.RfcConfigParameters RfcConfig)
+					{
+						return	SDM.GetDestination(RfcConfig);
+					}
+
+			#endregion
+
+		}
+}
