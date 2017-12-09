76c919ad69c06b40dbbfd30f9caf32c41fc21935
DPB <dbcm@mweb.co.za>
2017-12-09 18:46:36 +0200

    UPDATE

---
+++ BxSLib/BxS_Utilities/BxS_ToolSet/Code/Queue/Queue.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Concurrent;
+//•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+namespace BxS_Toolset.Queue
+{
+	public class Queue<T> where T : class
+		{
+			#region "Constructors"
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				public Queue()
+					{
+						this._queue		= new ConcurrentQueue<T>();
+					}
+
+			#endregion
+
+			//===========================================================================================
+			#region "Declarations"
+
+				private readonly ConcurrentQueue<T>		_queue;
+
+			#endregion
+
+			//===========================================================================================
+			#region "Properties"
+
+				public int Count	{ get { return this._queue.Count; } }
+
+			#endregion
+
+			//===========================================================================================
+			#region "Methods: Exposed"
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				public T Get()
+				{
+					this._queue.TryDequeue(out T lo_Obj);
+					return	lo_Obj	?? null;
+				}
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				public void Add(T item)
+				{
+					this._queue.Enqueue(item);
+				}
+
+			#endregion
+
+		}
+}
---
+++ BxSLib/BxS_Utilities/BxS_ToolSet/Code/Queue/QueueManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Concurrent;
+//•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+namespace BxS_Toolset.Queue
+{
+	public class QueueManager<T> where T : class
+		{
+			#region "Constructors"
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				public QueueManager(int NoQueues = 3)
+					{
+						this._quecount	= NoQueues;
+						this.Setup();
+					}
+
+			#endregion
+
+			//===========================================================================================
+			#region "Declarations"
+
+				private int		_quecount;
+				//.................................................
+				private readonly Lazy<Queue<T>>	_Q00		= new Lazy<Queue<T>>( new Queue<T>() );
+				private readonly Lazy<Queue<T>>	_Q01		= new Lazy<Queue<T>>( new Queue<T>() );
+				private readonly Lazy<Queue<T>>	_Q02		= new Lazy<Queue<T>>( new Queue<T>() );
+				private readonly Lazy<Queue<T>>	_Q03		= new Lazy<Queue<T>>( new Queue<T>() );
+				private readonly Lazy<Queue<T>>	_Q04		= new Lazy<Queue<T>>( new Queue<T>() );
+				private readonly Lazy<Queue<T>>	_Q05		= new Lazy<Queue<T>>( new Queue<T>() );
+				private readonly Lazy<Queue<T>>	_Q06		= new Lazy<Queue<T>>( new Queue<T>() );
+				private readonly Lazy<Queue<T>>	_Q07		= new Lazy<Queue<T>>( new Queue<T>() );
+				private readonly Lazy<Queue<T>>	_Q08		= new Lazy<Queue<T>>( new Queue<T>() );
+				private readonly Lazy<Queue<T>>	_Q09		= new Lazy<Queue<T>>( new Queue<T>() );
+				private readonly Lazy<Queue<T>>	_Q10		= new Lazy<Queue<T>>( new Queue<T>() );
+				//private ConcurrentDictionary<int, Queue<T>>	_queue;
+
+			#endregion
+
+			//===========================================================================================
+			#region "Properties"
+
+				public int Count	{ get { return this._queue.Count; }	}
+
+			#endregion
+
+			//===========================================================================================
+			#region "Methods: Exposed"
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				public T Get()
+				{
+					this._queue.TryDequeue(out T lo_Obj);
+					return	lo_Obj	?? null;
+				}
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				public void Add(T item)
+				{
+					this._queue.Enqueue(item);
+				}
+
+			#endregion
+
+			//===========================================================================================
+			#region "Methods: Private"
+
+				private void Setup()
+					{
+						if			(this._quecount	< 1)		this._quecount	= 1		;
+						else if (this._quecount> 10)		this._quecount	= 10	;
+						//.............................................
+						this._queue		= new ConcurrentDictionary<int, Queue<T>>();
+
+						for (int i = 0; i < this._quecount; i++)
+							{
+								this._queue.TryAdd(i, new Queue<T>());
+							}
+					}
+
+			#endregion
+
+		}
+}
