a212d2e9e5a5378ac9b6333dbca371a8578ccc25
DPB <dbcm@mweb.co.za>
2018-01-31 21:53:03 +0200

    update

---
+++ BxSLib/BxS_Utilities/zBxS_UT_SAPNCO/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("zBxS_UT_SAPNCO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("zBxS_UT_SAPNCO")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6eeb54b3-da72-43da-9a10-2e658efe4fe0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BxSLib/BxS_Utilities/zBxS_UT_SAPNCO/Test Units/UT_BxS_SAPNCO_20SAPINI.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//.........................................................
+using SMC	= SAP.Middleware.Connector;
+//.........................................................
+using BxS_SAPNCO.Destination;
+using BxS_SAPNCO.Helpers;
+using BxS_SAPNCO.API.DL;
+//•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+namespace zBxS_SAPNCO_UT
+{
+	[TestClass]
+	public class UT_BxS_SAPNCO_20SAPINI
+	{
+			//-------------------------------------------------------------------------------------------
+			[TestMethod]
+			public void UT_SAPNCO_SapIni()
+				{
+					//int						ln_Cnt	= 0;
+					//IList<IDTORefEntry> lt_Lst;
+					////...............................................
+					//ln_Cnt	++;
+
+					//var	lo_Ini	= new SAPLogonINI();
+					//var	lo_Rep	= new DestinationRepository();
+
+					//Assert.IsNotNull	(	lo_Ini	,	$"SAPNCO:Repos:SAPINI {ln_Cnt}: INI"		);
+					//Assert.IsNotNull	(	lo_Rep	,	$"SAPNCO:Repos:SAPINI {ln_Cnt}: Rep"		);
+					////...............................................
+					//ln_Cnt	++;
+
+					//lo_Ini.LoadRepository(lo_Rep);
+					//lt_Lst	= lo_Rep.ReferenceList();
+
+					//Assert.AreNotEqual	(0	, lo_Rep.Count	,	$"SAPNCO:Repos:SAPINI  {ln_Cnt}: List1");
+					////...............................................
+					//ln_Cnt	++;
+
+					//SMC.RfcConfigParameters lo_Cn1	= lo_Rep.GetParameters(lt_Lst[0].Name);
+
+					//Assert.AreNotEqual	(0	, lo_Cn1.Count	,	$"SAPNCO:Repos:SAPINI  {ln_Cnt}: List"	);
+				}
+	}
+}
