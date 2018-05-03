a31da0d55c7deaba2dad259050396326cb9ea8a3
DPB <dbcm@mweb.co.za>
2018-05-03 20:29:46 +0200

    update

---
+++ BxS_Worx/BxS_WorxUtilities/Code Repository/General/TopTenList.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+//•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+namespace BxS_WorxUtil.General
+{
+	public class TopTenList<T>
+		{
+			#region "Constructors"
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				public TopTenList( int	Size = 10 )
+					{
+						this.Size		= Size	;
+						//.............................................
+						this._List	= new	List<T>(Size);
+					}
+
+			#endregion
+
+			//===========================================================================================
+			#region "Declarations"
+
+				private IList<T>	_List;
+				private	int	_Size;
+
+			#endregion
+
+			//===========================================================================================
+			#region "Properties"
+
+				public	int Size	{ get { return	this._Size; }
+														set { this.Resize(value); } }
+
+				public	int	Count	{ get { return	this._List.Count; } }
+				//...
+				public	IList<T>	List	{ get {	return	this._List; } }
+
+			#endregion
+
+			//===========================================================================================
+			#region "Methods: Exposed"
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				public void Add( T value)
+					{
+						if ( !this._List.Count.Equals(0) )
+							{
+								if ( this._List[0].Equals(value) )	return;
+								//.............................................
+								for ( int i = 1; i < this.Count ; i++ )
+									{
+										if ( this._List[i].Equals(value) )
+											{
+												this._List.RemoveAt(i);
+												break;
+											}
+									}
+								//.............................................
+								if ( this._List.Count.Equals( this.Size ) )
+									{
+										this._List.RemoveAt( this.Size - 1 );
+									}
+							}
+						//.............................................
+						this._List.Insert(0,value);
+					}
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				public void Remove( T value)
+					{
+						try
+							{	this._List.RemoveAt( this._List.IndexOf( value ) );	}
+						catch
+							{	}
+					}
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				public void Clear()
+					{
+						this._List.Clear();
+					}
+
+			#endregion
+
+			//===========================================================================================
+			#region "Methods: Private"
+
+				//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+				private void Resize( int size )
+					{
+						if ( size < this.Size )
+							{
+								int s = this.Count - 1;
+								for ( int i = s; i >= size; i-- )
+									{
+										this._List.RemoveAt( i );
+									}
+							}
+						//...
+						this._Size	= size;
+					}
+
+			#endregion
+
+		}
+}
---
+++ BxS_Worx/BxS_zWorxUtil_UT/Test Units/UT_100_TopTen.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BxS_WorxUtil.General;
+
+namespace BxS_zWorx_UT_Destination.Test_Units
+{
+	[TestClass]
+	public class UT_100_TopTen
+		{
+			private	readonly TopTenList<string>	co_TT;
+
+			//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+			public UT_100_TopTen()
+				{
+					this.co_TT	= new TopTenList<string>();
+				}
+
+			[TestMethod]
+			//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+			public void UT_100_TopTen_10_Instantiate()
+				{
+					Assert.IsNotNull	( this.co_TT	, "" );
+				}
+
+			[TestMethod]
+			//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+			public void UT_100_TopTen_20_Add()
+				{
+					this.co_TT.Add("A");
+					Assert.AreEqual( 1 , this.co_TT.Count	, "" );
+					this.co_TT.Add("B");
+					Assert.AreEqual( 2 , this.co_TT.Count	, "" );
+					Assert.IsTrue( this.co_TT.List[0].Equals("B") , "");
+					this.co_TT.Add("C");
+					this.co_TT.Add("B");
+					Assert.AreEqual	( 3 , this.co_TT.Count								, ""	);
+					Assert.IsTrue		(			this.co_TT.List[0].Equals("B")	, ""	);
+
+					for ( int i = 0; i < 10; i++ )
+						{
+							this.co_TT.Add(i.ToString());
+						}
+					Assert.AreEqual	( this.co_TT.Size , this.co_TT.Count	, "" );
+					Assert.IsTrue		(	this.co_TT.List[0].Equals("9")	, ""	);
+					this.co_TT.Add("4");
+					Assert.AreEqual	( this.co_TT.Size , this.co_TT.Count	, "" );
+					Assert.IsTrue		(	this.co_TT.List[0].Equals("4")	, ""	);
+				}
+
+			[TestMethod]
+			//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+			public void UT_100_TopTen_30_Remove()
+				{
+					for ( int i = 0; i < 10; i++ )
+						{
+							this.co_TT.Add(i.ToString());
+						}
+					Assert.AreEqual	( this.co_TT.Size		, this.co_TT.Count	, "" );
+					this.co_TT.Remove("4");
+					Assert.AreEqual	( this.co_TT.Size-1 , this.co_TT.Count	, "" );
+					this.co_TT.Remove("$");
+					Assert.AreEqual	( this.co_TT.Size-1 , this.co_TT.Count	, "" );
+					this.co_TT.Clear();
+					Assert.AreEqual	( 0									, this.co_TT.Count	, "" );
+				}
+
+			[TestMethod]
+			//¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+			public void UT_100_TopTen_40_Resize()
+				{
+					for ( int i = 0; i < 10; i++ )
+						{
+							this.co_TT.Add(i.ToString());
+						}
+					Assert.AreEqual	( this.co_TT.Size , this.co_TT.Count	, "" );
+					this.co_TT.Size = 5;
+					Assert.AreEqual	( this.co_TT.Size , this.co_TT.Count	, "" );
+					Assert.AreEqual	( 5								, this.co_TT.Count	, "" );
+					this.co_TT.Size = 10;
+					for ( int i = 0; i < 10; i++ )
+						{
+							this.co_TT.Add(i.ToString());
+						}
+					Assert.AreEqual	( this.co_TT.Size , this.co_TT.Count	, "" );
+			}
+
+		//.
+
+		}
+
+	//.
+
+	}
