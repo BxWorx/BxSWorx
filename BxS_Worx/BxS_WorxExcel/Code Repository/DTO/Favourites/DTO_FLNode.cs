31a0e6d33546baf6ba0814c1d9c38ebb09c8663f
Derrick Bingham <dbcm@mweb.co.za>
2018-05-05 12:26:27 +0200

    update

---
+++ BxS_Worx/BxS_WorxExcel/Code Repository/DTO/Favourites/DTO_FLNode.cs
@@ -0,0 +1,20 @@
+﻿using System.Security;
+//•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+namespace BxS_WorxExcel.DTO
+{
+	internal struct DTO_FLNode
+		{
+			#region "Properties"
+
+				//...
+				internal	string	SAPID		{ get; set; }
+				internal	string	Client	{ get; set; }
+				internal	string	User		{ get; set; }
+				internal	SecureString	Pwrd		{ get; set; }
+				//...
+				internal	bool		IsSSO		{ get; set; }
+
+			#endregion
+
+		}
+	}
