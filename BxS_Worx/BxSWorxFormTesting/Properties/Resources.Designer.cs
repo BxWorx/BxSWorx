7b0c05bc5ee01f4012c9cfb255b16edda48f1d2a
Derrick Bingham <dbcm@mweb.co.za>
2018-05-21 19:18:52 +0200

    update

--- BxS_Worx/BxSWorxFormTesting/Properties/Resources.Designer.cs
+++ BxS_Worx/BxSWorxFormTesting/Properties/Resources.Designer.cs
@@ -73,9 +73,49 @@ namespace BxSWorxFormTesting.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icons8_Menu_25px {
+            get {
+                object obj = ResourceManager.GetObject("icons8_Menu_25px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_Microsoft_Excel_25px_1 {
+            get {
+                object obj = ResourceManager.GetObject("icons8_Microsoft_Excel_25px_1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icons8_Minimize_Window_25px {
             get {
                 object obj = ResourceManager.GetObject("icons8_Minimize_Window_25px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_Paint_Palette_25px {
+            get {
+                object obj = ResourceManager.GetObject("icons8_Paint_Palette_25px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_SAP_25px {
+            get {
+                object obj = ResourceManager.GetObject("icons8_SAP_25px", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
