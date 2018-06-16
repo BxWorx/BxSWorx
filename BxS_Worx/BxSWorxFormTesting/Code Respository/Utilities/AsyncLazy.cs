ff9ee19ad570d2b12fca8a029cfc5851c03e2c95
DPB <dbcm@mweb.co.za>
2018-06-16 10:13:49 +0200

    update

---
+++ BxS_Worx/BxSWorxFormTesting/Code Respository/Utilities/AsyncLazy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BxSWorxFormTesting.Code_Respository.Utilities
+	{
+	class AsyncLazy
+		{
+		}
+	}
+public class AsyncLazy<T> : Lazy<Task<T>> 
+{ 
+		public AsyncLazy(Func<T> valueFactory) : 
+				base(() => Task.Factory.StartNew(valueFactory)) { }
+
+		public AsyncLazy(Func<Task<T>> taskFactory) : 
+				base(() => Task.Factory.StartNew(() => taskFactory()).Unwrap()) { } 
+
+		public TaskAwaiter<T> GetAwaiter() { return Value.GetAwaiter(); } 
+}
